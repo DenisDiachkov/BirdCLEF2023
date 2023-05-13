ce67f8df60804c2bc2ff7dac8672f898600a333e
denis <diachkov.da@gmail.com>
2023-05-13 23:25:06 +0200

    отчаянная попытка завести jit-чекпоинтинг

--- codes/train.py
+++ codes/train.py
@@ -1,6 +1,9 @@
 import os
 from email import utils
+from typing import Any, Dict, Optional
+from lightning_fabric.utilities.types import _PATH
 
+import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as sched
@@ -9,7 +12,9 @@ import pytorch_lightning as pl
 from dataset.datamodule import DataModule
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.plugins import CheckpointIO
 from pytorch_lightning.utilities import rank_zero_only
+# from pl_bolts.callbacks import ORTCallback
 
 
 # Crutch for PyTorch Lightning to stop logging useless metrics
@@ -22,32 +27,63 @@ class _Wandblogger(WandbLogger):
 
 
 class LogCodeAndConfigCallback(pl.Callback):
-    def __init__(self, cfg) -> None:
+    def __init__(self, logger_params) -> None:
         super().__init__()
-        self.cfg = cfg
+        self.logger_params = logger_params
 
     @rank_zero_only
     def on_fit_start(self, trainer, pl_module):
         trainer.logger.experiment.log_code(
             root=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         )
-        trainer.logger.experiment.config.update({**self.cfg})
+        trainer.logger.experiment.config.update({**self.logger_cfg}, allow_val_change=True)
 
 
+# class SaveTorchScriptCheckpointIO(CheckpointIO):
+#     def __init__(self, pl_module: str, pl_module_params: dict, dummy_input) -> None:
+#         super().__init__()
+#         print(pl_module, pl_module_params)
+#         self.pl_module:pl.LightningModule = utils.get_obj(pl_module)(pl_module_params)
+#         self.dummy_input = dummy_input
+
+#     def save_checkpoint(self, checkpoint, path, storage_options=None):
+#         # Save jit
+#         self.pl_module.load_state_dict(checkpoint['state_dict'])
+#         traced = torch.jit.trace(
+#             self.pl_module.model, self.dummy_input, strict=False
+#         )
+#         traced.save(path.replace('.ckpt', '_jit.pt'))
+#         # Save .ckpt
+#         super().save_checkpoint(checkpoint, path, storage_options=storage_options)
+    
+#     def load_checkpoint(self, path, map_location: Any | None = None) -> Dict[str, Any]:
+#         return super().load_checkpoint(path, map_location)
+
+#     def remove_checkpoint(self, path) -> None:
+#         super().remove_checkpoint(path.replace('.ckpt', '_jit.pt'))
+#         super().remove_checkpoint(path)
+        
 
 def train(cfg: dict):
     if not os.path.exists(cfg.experiment_path):
         os.makedirs(cfg.experiment_path)
-    logger = _Wandblogger(**cfg.logger_params)
+    logger = _Wandblogger(**cfg.logger_params) if not cfg.no_logging else None
     callbacks = [
         utils.get_obj(callback.callback)(
             **callback.callback_params | ({"dirpath": cfg.experiment_path} if callback.callback.endswith("ModelCheckpoint") else {})
         ) 
         for callback in cfg.trainer_callbacks
-    ] + [LogCodeAndConfigCallback(cfg)]
+    ]
+    if not cfg.no_logging:
+        callbacks.append(LogCodeAndConfigCallback(cfg))
     trainer = Trainer(
         logger=logger,
         callbacks=callbacks,
+        # plugins=[SaveTorchScriptCheckpointIO(
+        #     cfg.lightning_module,
+        #     cfg.lightning_module_params,
+        #     utils.get_obj(cfg.datamodule_params.dataset).dummy_input()
+        # )],
         **cfg.trainer_params
     )
     trainer.fit(
